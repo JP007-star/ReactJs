06b81afc5ec1ae8e81de1bb66b88f5452bd55967
JP007-Star <justice_of_peace@yahoo.com>
2022-05-10 01:29:31 +0530

    forms

--- react-advanced-2020-master/src/App.js
+++ react-advanced-2020-master/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import Setup from './tutorial/3-conditional-rendering/setup/2-short-circuit'
+import Setup from './tutorial/4-forms/setup/2-multiple-inputs'
 
 function App() {
+
   return (
     <div className='container'>
       {/* <h2>Advanced Tutorial</h2> */}
